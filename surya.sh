1dc2c7755de4e2df60ecee5c5203b086734275ad
iqbal45778 <163950305+iqbal45778@users.noreply.github.com>
2024-04-15 16:18:09 +0700

    Update surya.sh

--- surya.sh
+++ surya.sh
@@ -110,7 +110,7 @@ makekernel() {
 	CROSS_COMPILE="${KERNELDIR}/gcc/bin/aarch64-none-linux-gnu-" \
 	CROSS_COMPILE_COMPAT="${KERNELDIR}/gcc32/bin/arm-none-linux-gnueabihf-"
     else
-	    make -j$(nproc --all) O=out ARCH=arm64 CROSS_COMPILE="${KERNELDIR}/gcc/bin/aarch64-elf-" CROSS_COMPILE_ARM32="${KERNELDIR}/gcc32/bin/arm-eabi-"
+	    make -j$(nproc --all) O=out ARCH=arm64 CROSS_COMPILE=aarch64-elf- CROSS_COMPILE_ARM32=arm-eabi- LD=${KERNELDIR}/bin/aarch64-elf-ld AR=aarch64-elf-ar AS=aarch64-elf-as NM=aarch64-elf-nm OBJDUMP=aarch64-elf-objdump OBJCOPY=aarch64-elf-objcopy CC=aarch64-elf-gcc
     fi
 
     # Check if compilation is done successfully.
